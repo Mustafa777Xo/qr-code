fece6d42e9bcad0410760e67d61b555a7bf55391
Mustafa777Xo <mustafa777bdo@gmail.com>
2023-07-30 15:43:24 +0200

    qr-code init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app class="app-background">
+    <router-view />
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
+<style scoped>
+.app-background {
+  background-color: #d6e2f0;
+}
+</style>
---
+++ src/components/QrCard.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container class="pa-16">
+    <v-row>
+        <v-col>
+            <v-img src="../assets/image-qr-code.png" width="25rem"></v-img>
+        </v-col>
+    </v-row>    
+</v-container>
+</template>
